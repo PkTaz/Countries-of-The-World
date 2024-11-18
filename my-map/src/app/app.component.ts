f8a920be25690c1ad851e9b07f9c3d4848ec76cb
wchap23 <wchap23@wgu.edu>
2024-11-18 15:02:14 -0500

    updated routerModule

--- my-map/src/app/app.component.ts
+++ my-map/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WorldComponent } from "./world/world.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WorldComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'd280_app';
